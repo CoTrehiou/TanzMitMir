8e8cbff14990973a4590ccca5d22da0436fa3952
Papouboy <corentin.trehiou@wanadoo.fr>
2020-04-13 13:14:09 +0200

    [UPDATE] Spider Move

--- Assets/Gameplay/Spider/Scripts/Spider.cs
+++ Assets/Gameplay/Spider/Scripts/Spider.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,24 +7,68 @@ public abstract class Spider : MonoBehaviour
 {
 
 
-    protected int _health;
+    protected int _health = 100;
     protected int _damage = 50;
 
-    protected bool alive = true;
+    public bool alive = true;
 
-    protected int team;
+    protected int _team;
 
-    protected struct position
+    protected int _moveSpeed;
+
+    public  struct OptionsMovement
     {
-        int X;
-        int Y;
+        public Position UP;
+        public Position DOWN;
+        public Position LEFT;
+        public Position RIGHT;
+    }
+    public struct Position
+    {
+        public int X;
+        public int Y;
+    }
+    protected OptionsMovement _optionsMovement;
+    public Position _currentPosition;
+
+    protected void SpiderInit(Position positionInitial, int health, int damage, int team, int moveSpeed)
+    {
+        _currentPosition = positionInitial;
+        _health = health;
+        _damage = damage;
+        _team = team;
+        _moveSpeed = moveSpeed;
     }
 
-
-    protected void SpiderInit()
+    protected void MoveNextPosition()
     {
+        // Position nextPosition = new Position(Random.Range(_moveSpeed, - _moveSpeed), Random.Range(_moveSpeed, -_moveSpeed));
+        int nextPosition = UnityEngine.Random.Range(0, 4);
+        Debug.Log(nextPosition);
+        switch (nextPosition)
+        {
+            case 0:
+                Interact(_optionsMovement.UP);
+                break;
+            case 1:
+                Interact(_optionsMovement.DOWN);
+                break;
+            case 2:
+                Interact(_optionsMovement.LEFT);
+                break;
+            case 3:
+                Interact(_optionsMovement.RIGHT);
+                break;
+            default:
+                Debug.Log("Error return value");
+                break;
+
+        }
 
     }
 
-
+    private void Interact(Position typePosition)
+    {
+        throw new NotImplementedException();
+    }
 }
