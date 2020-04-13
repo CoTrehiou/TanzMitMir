b2031335aa5d41f40b0095c0f72e988e83dc98c2
Papouboy <corentin.trehiou@wanadoo.fr>
2020-04-13 12:16:50 +0200

    [ADD] Class spider

---
+++ Assets/Gameplay/Spider/Scripts/Spider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Spider : MonoBehaviour
+{
+
+
+    protected int _health;
+    protected int _damage = 50;
+
+    protected bool alive = true;
+
+    protected int team;
+
+    protected struct position
+    {
+        int X;
+        int Y;
+    }
+
+
+    protected void SpiderInit()
+    {
+
+    }
+
+
+}
