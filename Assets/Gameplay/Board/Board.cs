821c3811d494be84966ce37228b97ff73552cd91
Papouboy <corentin.trehiou@wanadoo.fr>
2020-04-13 15:29:29 +0200

    [ADD] Board

---
+++ Assets/Gameplay/Board/Board.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+
+    [SerializeField] Transform _planeGround;
+    [SerializeField] int width = 10;
+    [SerializeField] int height = 10;
+
+
+
+
+    void Awake()
+    {
+        _planeGround.localScale = new Vector3(width,0, height);
+    }
+
+    /*
+    TileState Tile(Position position) {
+        if()
+    }
+    */
+}
+
+public class Tile{
+    public Position _positionTile;
+    public TileState _tileState;
+
+    public void InitTile(int positionX, int positionY)
+    {
+        _positionTile.X = positionX;
+        _positionTile.Y = positionY;
+    }
+    public enum TileState
+    {
+        Empty,
+        Unit,
+        Wall
+    }
+
+    TileState GetTile(Position position)
+    {
+        return _tileState;
+    }
+}
+
+
