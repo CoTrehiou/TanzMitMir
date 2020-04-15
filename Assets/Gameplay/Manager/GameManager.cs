c41153bc4705916be158dabdf691292a193732d5
Papouboy <corentin.trehiou@wanadoo.fr>
2020-04-15 20:52:38 +0200

    [ [ADD]  Method GameManager Spawn Soldier and egg

--- Assets/Gameplay/Manager/GameManager.cs
+++ Assets/Gameplay/Manager/GameManager.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
     public static GameManager instance;
 
-    public Color teamRed = Color.red;
-    public Color teamBlue = Color.blue;
+    public Color colorTeamRed = Color.red;
+    public Color colorTeamBlue = Color.blue;
+
+    [Header("Queen")]
+    [SerializeField] int _damageQueen = 100;
+
 
     [Header("Soldier")]
     [SerializeField] GameObject _soldier;
+    [SerializeField] int  _soldierHealth = 100;
+    [SerializeField] int _soldierDamage  = 50;
+    [SerializeField] int _soldierMoveSpeed = 1;
+
+    [Header("Egg")]
+    [SerializeField] GameObject _egg;
+
+
+
 
 
     //public readonly List<Unit> unitsList = new List<Unit>();
@@ -23,8 +37,6 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-      //  GameObject o = Instantiate(_soldier);
-      //  o.GetComponent<Soldier>().Init();
 
     }
 
@@ -32,5 +44,20 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void SpawnEgg(Position _currentPosition, Unit.Team _team)
+    {
+        Vector3 positionEgg = new Vector3(_currentPosition.X, 0, _currentPosition.Y);
+        GameObject o = Instantiate(_egg, positionEgg, Quaternion.identity);
+
+        o.GetComponent<Egg>().eggTeam = _team;
+        o.GetComponent<Egg>().Renderer();
+    }
+
+    internal void SpawnSoldier(Vector3 position, Unit.Team eggTeam)
+    {
+        GameObject o = Instantiate(_soldier, position, Quaternion.identity);
+        o.GetComponent<Soldier>().Init(_soldierHealth, _soldierHealth, _soldierMoveSpeed, eggTeam);
     }
 }
